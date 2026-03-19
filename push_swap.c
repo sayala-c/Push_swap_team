abf8c5244c3520f6bb2da89f9abd87a192303d72
Oscar Duran Marti <oduran-m@cbr4s3.42barcelona.com>
2026-03-20 00:18:05 +0100

    casi parseo

--- push_swap.c
+++ push_swap.c
@@ -6,15 +6,23 @@
 /*   By: oduran-m <oduran-m@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 19:45:29 by oduran-m          #+#    #+#             */
-/*   Updated: 2026/03/19 22:20:47 by oduran-m         ###   ########.fr       */
+/*   Updated: 2026/03/20 00:17:27 by oduran-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
+
 int	main(int argc, char **argv)
 {
+	if (argc <= 1)
+		return (ft_printf("Error"), NULL);
+	else if (argc = 2)
+		parseosplit(argv[1]);
+	else if (argc > 2)
+		parseo(argv);
+		
 	ft_printf("argc: %d\nargv: %s\n", argc, argv[1]);
 	return (0);
 }
