8b02354f490e7596f80e5f229dd23bce806bad5f
sayala-c <sara.ayala.carrillo@gmail.com>
2026-04-06 09:13:16 +0200

    Implement value_index function for stack indexing
    Add value_index function to assign indices to stack elements

--- push_swap_ejec.c
+++ push_swap_ejec.c
@@ -20,3 +20,28 @@ int	swap_ejecution(t_stack **a, t_flag *flags)
 		ft_printf("ordenado\n");
 	return (0);
 }
+
+void	value_index(t_stack **a)
+{
+	int	size;
+	int	i;
+	t_stack	 *min_node;
+	t_stack	*current;
+	
+	i = 0;
+	size = stack_size(*a);
+	while (i < size)
+	{
+		current = *a;
+		min_node = NULL;
+		while (current != NULL)
+		{
+			if (current->index == -1  && (min_node == NULL || current->value < min_node->value))
+				min_node = current;
+			current = current->next;
+		}
+			if (min_node != NULL)
+				min_node->index = i;
+		i++;
+	}
+}
