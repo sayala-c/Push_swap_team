a374f44c5d1ccf6f948dd1badd3f90e56e771ccd
sayala-c <sara.ayala.carrillo@gmail.com>
2026-04-06 11:15:53 +0200

    Improve comment clarity in complex_algorithm.c
    Clarified the comment explaining the bit shifting logic for determining the number of iterations needed in the algorithm.

--- complex_algorithm.c
+++ complex_algorithm.c
@@ -35,7 +35,7 @@ void	radix_sort(t_stack **a, t_stack **b)
 
 	size = stack_size(*a);
 	max_bits = 0;
-	while (((size - 1) >> max_bits) != 0)
+	while (((size - 1) >> max_bits) != 0)//si tenemos 7 elementos (0 a 6) el 6 es el index maximo que podremos tener. Si 6 lo desplazamos x max_bits es = a nulo? No - sigue en bucle. (convierte el size en binario y va desplazando bites segun el max_bit hasta que quede en todo 0). Asi sabemos cuantas vueltas hay que dar, es decir cuántas veces tienes que repetir el proceso de mandar a B y devolver a A.
 		max_bits++;
 	i = 0;
 	while (i < max_bits)
