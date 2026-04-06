bd2557e02030b69307e54ccd06b1e281def7c0c5
Oscar Duran Marti <oduran-m@cbr3s1.42barcelona.com>
2026-04-06 20:13:32 +0200

    add benchmarck

--- push_swap_ejec.c
+++ push_swap_ejec.c
@@ -6,19 +6,52 @@
 /*   By: oduran-m <oduran-m@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/05 18:18:44 by oduran-m          #+#    #+#             */
-/*   Updated: 2026/04/06 19:23:56 by oduran-m         ###   ########.fr       */
+/*   Updated: 2026/04/06 20:11:33 by oduran-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static const char	*method_name(t_method m)
+{
+	if (m == METHOD_SIMPLE)
+		return ("Simple / O(n2)");
+	if (m == METHOD_MEDIUM)
+		return ("Medium / O(n√n)");
+	if (m == METHOD_COMPLEX)
+		return ("Complex / O(nlogn)");
+	if (m == METHOD_ADAPTIVE)
+		return ("Adaptive / O(n√n)");
+	return ("NONE");
+}
+
+void	benchmark(t_flag *flags, t_datacount *data, double disorder)
+{
+	ft_printf("[bench] disorder: %d%%\n", disorder);
+	ft_printf("[bench] strategy: %s\n", method_name(flags->method));
+	ft_printf("[bench] total_ops: %d%%\n", data->total_operations);
+	ft_printf("[bench] sa: %d ", data->sa);
+	ft_printf("sb: %d ", data->sb);
+	ft_printf("ss: %d ", data->ss);
+	ft_printf("pa: %d ", data->pa);
+	ft_printf("pb: %d \n", data->pb);
+	ft_printf("[bench] ra: %d ", data->ra);
+	ft_printf("rb: %d ", data->rb);
+	ft_printf("rr: %d ", data->rr);
+	ft_printf("rra: %d ", data->rra);
+	ft_printf("rrb: %d ", data->rrb);
+	ft_printf("rrr: %d \n", data->rrr);
+}
+
 int	swap_ejecution(t_stack **a, t_flag *flags)
 {
 	t_stack		*stack_b;
 	t_datacount	datacount;
+	double		disorder;
 	
+	disorder = disorder_index(*a);
 	stack_b = NULL;
-	if (disorder_index(*a) == 0)
+	if (disorder == 0)
 		ft_printf("ordenado\n");
 	else if (flags->method == METHOD_SIMPLE)
 		ft_printf("method simple\n");
@@ -29,11 +62,12 @@ int	swap_ejecution(t_stack **a, t_flag *flags)
 	else if (flags->method == METHOD_ADAPTIVE)
 		ft_printf("method adaptive\n");
 	if (flags->bench == 1)
-		ft_printf("benchmark\n");
+		benchmark(flags, &datacount, disorder);
 		
 	
 	return (0);
 }
+
 
 void	value_index(t_stack **a)
 {
