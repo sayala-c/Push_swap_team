e751e189cff17edad79c06229e86165b4e9be29a
Sara Ayala Carrillo <sayala-c@cbr3s2.42barcelona.com>
2026-04-06 21:17:30 +0200

    init datastruct

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: oduran-m <oduran-m@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 19:45:29 by oduran-m          #+#    #+#             */
-/*   Updated: 2026/04/05 18:25:34 by oduran-m         ###   ########.fr       */
+/*   Updated: 2026/04/06 21:16:21 by sayala-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	main(int argc, char **argv)
 {
 	t_flag		opts;
 	t_stack		*stack_a;
+	t_datacount	*data;
 
 	init_flag(&opts);
 	stack_a = NULL;
@@ -25,6 +26,7 @@ int	main(int argc, char **argv)
 	if (parseo(argc, argv, &opts, &stack_a))
 		ft_printf("Error\n");
 	value_index(&stack_a);
+	init_datacount(&data);
 	if (swap_ejecution(&stack_a, &opts))
 		ft_printf("Error ejec\n");
 		
