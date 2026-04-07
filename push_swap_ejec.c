0453a2990d82afdb80be3d78fa5a646a3d44d7d3
Oscar Duran Marti <oduran-m@cbr4s1.42barcelona.com>
2026-04-07 21:14:53 +0200

    free echo, sin lickes

--- push_swap_ejec.c
+++ push_swap_ejec.c
@@ -6,12 +6,27 @@
 /*   By: oduran-m <oduran-m@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/05 18:18:44 by oduran-m          #+#    #+#             */
-/*   Updated: 2026/04/07 20:52:43 by oduran-m         ###   ########.fr       */
+/*   Updated: 2026/04/07 21:13:43 by oduran-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
+
+void	free_stack(t_stack **stack)
+{
+	t_stack *tmp;
+	
+	if (!stack || !*stack)
+		return ;
+	while (*stack)
+	{
+		tmp = (*stack)->next;
+		free(*stack);
+		*stack = tmp;
+	}
+	*stack = NULL;
+}
 
 void    select_simple(t_stack **a, t_stack **b, t_datacount *data)
 {
@@ -36,7 +51,7 @@ int	swap_ejecution(t_stack **a, t_flag *flags)
 	init_datacount(&datacount);
 	datacount.bench_mode = flags->bench;
 	if (disorder == 0)
-		return (0);
+		return (free_stack(a), 0);
 	else if (flags->method == METHOD_SIMPLE)
 		select_simple(a, &stack_b, &datacount);
 	else if (flags->method == METHOD_MEDIUM)
@@ -47,7 +62,7 @@ int	swap_ejecution(t_stack **a, t_flag *flags)
 		adaptative_alg(a, &stack_b, &datacount);
 	if (flags->bench == 1)
 		benchmark (flags, &datacount, disorder);
-	return (0);
+	return (free_stack(a), free_stack(&stack_b), 0);
 }
 
 void	value_index(t_stack **a)
